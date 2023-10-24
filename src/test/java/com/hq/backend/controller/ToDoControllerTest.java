4018c42d66737fdc9bcc6e420f57c79b3381e7fc
unknown <arun.bhaud@arrkgroup.com>
2023-10-25 01:26:24 +0530

    Added sample test classes

---
+++ src/test/java/com/hq/backend/controller/ToDoControllerTest.java
@@ -0,0 +1,14 @@
+package com.hq.backend.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ToDoControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
