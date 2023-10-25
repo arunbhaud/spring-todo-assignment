614f20c5d19d48968edc373802f5464f9071f11e
unknown <arun.bhaud@arrkgroup.com>
2023-10-25 14:55:54 +0530

    added 2 test cases

--- src/test/java/com/hq/backend/controller/ToDoControllerTest.java
+++ src/test/java/com/hq/backend/controller/ToDoControllerTest.java
@@ -1,14 +1,56 @@
 package com.hq.backend.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hq.backend.entity.ToDo;
+import com.hq.backend.service.ToDoService;
+
+	
+@WebMvcTest(ToDoController.class)
 class ToDoControllerTest {
+	
+	@Autowired private MockMvc mockMvc;
+    
+    @Autowired private ObjectMapper objectMapper;
+     
+    @MockBean private ToDoService service;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testAddToDoSuccess() throws Exception {
+    	ToDo todo1 = new ToDo(1, "Excercise", false);
+    	
+ 
+        String requestBody = objectMapper.writeValueAsString(todo1);
+        
+        when(service.addTodo(any())).thenReturn(true);
+ 
+        mockMvc.perform(post("/todo").contentType("application/json")
+                .content(requestBody))
+                .andExpect(status().isCreated());
+    }
+    
+    @Test
+    public void testAddToDoFailure() throws Exception {
+    	ToDo todo1 = new ToDo(1, "Excercise", false);
+    	
+ 
+        String requestBody = objectMapper.writeValueAsString(todo1);
+        
+        when(service.addTodo(any())).thenReturn(false);
+ 
+        mockMvc.perform(post("/todo").contentType("application/json")
+                .content(requestBody))
+                .andExpect(status().isBadRequest());
+    }
 
 }
